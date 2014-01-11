6f6d6618db9fe6a1da3d7d4c068d39a0623fb1ce
cdeange <christiand@carezone.com>
2014-01-11 14:05:05 -0500

    Added width and height fields

--- gimgur/src/main/java/com/deange/gimgur/model/ImageResult.java
+++ gimgur/src/main/java/com/deange/gimgur/model/ImageResult.java
@@ -10,11 +10,25 @@ public class ImageResult {
     @SerializedName("contentNoFormatting")
     private String mDescription;
 
+    @SerializedName("width")
+    private int mWidth;
+
+    @SerializedName("height")
+    private int mHeight;
+
     public String getUrl() {
         return mUrl;
     }
 
     public String getDescription() {
         return mDescription;
+    }
+
+    public int getWidth() {
+        return mWidth;
+    }
+
+    public int getHeight() {
+        return mHeight;
     }
 }
